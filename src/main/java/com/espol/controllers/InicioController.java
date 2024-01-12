4cf0de335daf9defee4d8346e3e8ebcc05ed5f95
JuanFernandez <juaffern@espol.edu.ec>
2024-01-12 18:09:15 -0500

    Module of Imports/Exports changed

--- src/main/java/com/espol/controllers/InicioController.java
+++ src/main/java/com/espol/controllers/InicioController.java
@@ -1,7 +1,9 @@
 package com.espol.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class InicioController {
 
@@ -20,4 +22,9 @@ public class InicioController {
     @FXML
     private Button btnSalir;
 
+    @FXML
+    public void handleCloseButtonAction(ActionEvent event) {
+        Stage stage = (Stage) btnSalir.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,11 @@ module com.espol {
     requires javafx.fxml;
 
     opens com.espol to javafx.fxml;
+    opens com.espol.controllers to javafx.fxml;
+    opens com.espol.controllers.auspiciantes to javafx.fxml;
+    opens com.espol.controllers.emprendedores to javafx.fxml;
+    opens com.espol.controllers.ferias to javafx.fxml;
+    opens com.espol.controllers.stands to javafx.fxml;
     exports com.espol;
     exports com.espol.controllers;
     exports com.espol.controllers.auspiciantes;
