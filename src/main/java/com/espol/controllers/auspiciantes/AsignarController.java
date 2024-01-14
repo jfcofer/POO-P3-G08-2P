ad691bd8a500b412c66397aa82b2e68ef33e9d8b
William Mayorga <>
2024-01-13 20:11:58 -0500

    Creacion de los controladores para asignar y registrar auspiciantes

---
+++ src/main/java/com/espol/controllers/auspiciantes/AsignarController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.controllers.auspiciantes;
+
+import com.espol.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author William
+ */
+public class AsignarController {
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) {
+        App.setScreen("auspiciantes/tabla", event);
+    }
+    
+}
---
+++ src/main/resources/com/espol/controllers/auspiciantes/RegistrarController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.espol.controllers.auspiciantes;
+
+import com.espol.App;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author William
+ */
+public class RegistrarController implements Initializable {
+
+    @FXML
+    private Button btnRegresar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBackButtonAction(ActionEvent event) {
+        App.setScreen("auspiciantes/tabla", event);
+    }
+    
+}
