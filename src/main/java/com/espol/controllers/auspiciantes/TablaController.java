4e8973a1b0779c0c6c479d5f48fc41006b4634a1
Cris <Cris@DESKTOP-TRPM1ES>
2024-01-15 20:19:18 -0500

    Creacion del controlador para Tabla de Auspiciantes

--- src/main/java/com/espol/controllers/auspiciantes/TablaController.java
+++ src/main/java/com/espol/controllers/auspiciantes/TablaController.java
@@ -1,25 +1,39 @@
 package com.espol.controllers.auspiciantes;
 
 import com.espol.App;
+import com.espol.models.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
+
 
 public class TablaController {
 
     @FXML
     private Button asignarFeriaButton;
-
     @FXML
     private Button backButton;
-
     @FXML
     private Button registerButton;
-
     @FXML
-    private TableView<?> tabla;
+    private TableView<Auspiciante> tabla;
+    @FXML
+    private TableColumn columnaCedula;
+    @FXML
+    private TableColumn columnaNombre;
+    @FXML
+    private TableColumn columnaTelefono;
+    @FXML
+    private TableColumn columnaEmail;
 
     @FXML
     public void handleBackButtonAction(ActionEvent event) {
@@ -38,7 +52,20 @@ public class TablaController {
 
     @FXML
     public void initialize(){
-    
+        ArrayList<Auspiciante> auspiciantes = App.datos.getListaAuspiciantes();
+        
+        // Definir cómo se llenarán las columnas
+        columnaCedula.setCellValueFactory(new PropertyValueFactory<>("ruc"));
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        columnaTelefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        columnaEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+
+        // Convertir la lista de auspiciantes a una lista observable
+        ObservableList<Auspiciante> listaObservable = FXCollections.observableArrayList(auspiciantes);
+
+        // Añadir los datos a la tabla
+        tabla.setItems(listaObservable);
     }
 
 }
