33365c1371c9832850484bc89f112185df94b8c4
William Mayorga <>
2024-01-15 22:00:56 -0500

    Implementacion del controlador para Registrar Emprendedores

--- src/main/java/com/espol/controllers/emprendedores/RegistrarController.java
+++ src/main/java/com/espol/controllers/emprendedores/RegistrarController.java
@@ -7,7 +7,10 @@ package com.espol.controllers.emprendedores;
 import com.espol.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import com.espol.models.Emprendedor;
 
 /**
  * FXML Controller class
@@ -17,7 +20,76 @@ import javafx.scene.control.Button;
 public class RegistrarController {
 
     @FXML
+    private TextField CedulaTextField;
+
+    @FXML
+    private TextField DescripcionServiciosTextField;
+
+    @FXML
+    private TextField DireccionTextField;
+
+    @FXML
+    private TextField EmailFacebookTextField;
+    
+    @FXML
+    private TextField EmailTiktokTextField;
+
+    @FXML
+    private TextField EmailInstagramTextField;
+
+    @FXML
+    private TextField EmailLinkedinTextField;
+
+    @FXML
+    private TextField EmailPinterestTextField;
+
+    @FXML
+    private TextField EmailTextField;
+
+    @FXML
+    private TextField EmailTwitterTextField;
+
+    @FXML
+    private TextField EmailYoutubeTextField;
+
+    @FXML
+    private TextField NombreResponsableTextField;
+
+    @FXML
+    private TextField NombreTextField;
+
+    @FXML
+    private TextField SitioWebTextField;
+
+    @FXML
+    private TextField TelefonoTextField;
+
+    @FXML
+    private TextField UserFacebookTextField;
+
+    @FXML
+    private TextField UserInstagramTextField;
+
+    @FXML
+    private TextField UserLinkedinTextField;
+
+    @FXML
+    private TextField UserPinterestTextField;
+
+    @FXML
+    private TextField UserTiktokTextField;
+
+    @FXML
+    private TextField UserTwitterTextField;
+
+    @FXML
+    private TextField UserYoutubeTextField;
+
+    @FXML
     private Button btnRegresar;
+    
+    @FXML
+    private Button btnRegistrar;
 
     /**
      * Initializes the controller class.
@@ -29,6 +101,98 @@ public class RegistrarController {
     @FXML
     private void handleBackButtonAction(ActionEvent event) {
         App.setScreen("emprendedores/tabla", event);
+    }
+    
+    @FXML
+    void handleRegistrarEmprendedorButtonAction(ActionEvent event) {
+        
+        String cedula = CedulaTextField.getText();
+        String nombre = NombreTextField.getText();
+        String responsable = NombreResponsableTextField.getText();
+        String telefono = TelefonoTextField.getText();
+        String email = EmailTextField.getText();
+        String direccion = DireccionTextField.getText();
+        String sitioWeb = SitioWebTextField.getText();
+        String DescripcionServicio = DescripcionServiciosTextField.getText();
+        Boolean condicion = false;
+
+        // Verificar que no exista otro emprendedor con el mismo número de cédula o RUC
+        for (Emprendedor emprendedor : App.datos.getEmprendedores()) {
+            if (emprendedor.getRuc().equals(cedula)) {
+                condicion = true;
+                return;
+            }
+        }
+        
+        if (condicion == true || cedula.isBlank()) {
+            mostrarAlertaError("La cedula no puede quedar vacía o ya existe un emprendedor registrado con esa cédula o ruc");
+        } 
+        if(nombre.isBlank()) {
+            mostrarAlertaError("El nombre no puede quedar vacía");
+        }
+        else if(responsable.isBlank()){
+            mostrarAlertaError("El nombre de la persona responsable no puede quedar vacío");
+        }
+        else if(telefono.isBlank()){
+            mostrarAlertaError("El número de teléfono no puede quedar vacío");
+        }
+        else if(email.isBlank()){
+            mostrarAlertaError("El email no puede quedar vacío");
+        }
+        else if(DescripcionServicio.isBlank()){
+            mostrarAlertaError("La descripcion del servicio no puede quedar vacío");
+        }
+        else{
+            // Crear un nuevo objeto Emprendedor y agregarlo a la lista
+            Emprendedor nuevoEmprendedor = new Emprendedor(cedula, nombre, telefono, email, direccion, sitioWeb, responsable, DescripcionServicio);
+
+            // Agregar las redes , leyendo los text fields de cada campo
+            if (!UserTwitterTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("Twitter", UserTwitterTextField.getText(), EmailTwitterTextField.getText());
+            }
+            if (!UserTiktokTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("TikTok", UserTiktokTextField.getText(), EmailTiktokTextField.getText());
+            }
+            if (!UserFacebookTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("Facebook", UserFacebookTextField.getText(), EmailFacebookTextField.getText());
+            }
+            if (!UserInstagramTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("Instagram", UserInstagramTextField.getText(), EmailInstagramTextField.getText());
+            }
+            if (!UserYoutubeTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("Youtube", UserYoutubeTextField.getText(), EmailYoutubeTextField.getText());
+            }
+            if (!UserLinkedinTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("LinkedIn", UserLinkedinTextField.getText(), EmailLinkedinTextField.getText());
+            }
+            if (!UserPinterestTextField.getText().isEmpty()) {
+                nuevoEmprendedor.agregarRedSocial("Pinterest", UserPinterestTextField.getText(), EmailPinterestTextField.getText());
+            }
+            App.datos.getEmprendedores().add(nuevoEmprendedor);
+            mostrarAlertaInfo("El registro se ha realizado correctamente");
+            App.setScreen("emprendedores/tabla", event);
+            
+        }
+        
+
+    }
+    
+    @FXML
+    private void mostrarAlertaError(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setTitle("Error");
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void mostrarAlertaInfo(String mensaje){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Info");
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 
 }
