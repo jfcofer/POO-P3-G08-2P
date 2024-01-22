28da9328fa23a0d0cc84bbfa778138d4c5583071
Cris <Cris@DESKTOP-TRPM1ES>
2024-01-21 21:45:28 -0500

    Desarrollo de Stands Controller

--- src/main/java/com/espol/controllers/ferias/StandsController.java
+++ src/main/java/com/espol/controllers/ferias/StandsController.java
@@ -1,13 +1,29 @@
 package com.espol.controllers.ferias;
 
+import com.espol.App;
+import com.espol.controllers.stands.ReservarController;
 import com.espol.models.Feria;
+import com.espol.models.Seccion;
+import com.espol.models.Stand;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class StandsController {
 
+    @FXML
+    private Button backButton;
+    
     @FXML
     private TextArea infoTxtArea;
 
@@ -25,7 +41,30 @@ public class StandsController {
 
     @FXML
     public void initialize(Feria feria){
-        
+        HBox[] HBoxes = { sec1HBox, sec2HBox, sec3HBox, sec4HBox };
+        Seccion[] secciones = feria.getSecciones();
+        for (int i = 0; i < HBoxes.length; i++) {
+
+            for (Stand stand : secciones[i].getStands()) {
+                Label titulo = new Label("Stand");
+                Label codigo = new Label(stand.getCodigo());
+                VBox standVBox = new VBox(3);
+                VBox.setVgrow(standVBox, Priority.ALWAYS);
+                standVBox.setAlignment(Pos.CENTER);
+                standVBox.setPrefWidth(100);
+                standVBox.getChildren().addAll(titulo, codigo);
+                standVBox.setOnMouseClicked(event -> {
+                    infoTxtArea.setText(stand.toString());
+                });
+                
+                HBoxes[i].getChildren().add(standVBox);
+            }
+        }
+    }
+    
+    @FXML
+    public void handleBackButtonAction(ActionEvent event) {
+        App.setScreen("inicio", event);
     }
 
 }
